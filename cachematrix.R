2cd64a82dc7d6439669edbd65ba32475e4fbf161
Francois Badet <f.badet@free.fr>
2015-05-21 12:06:46 +0200

    Signed-off-by: Francois Badet <f.badet@free.fr>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function makeCacheMatrix creates a matrix
+## and a list of 4 functions (set, get, setinvx, getinvx)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ## Initialize matrice invx
+  invx = matrix()
+  invx <- NULL
+  
+  ## Function set caches the matrix in the parent environment (<<- operator)
+  ## and re-initialize the inverse matrix in the parent environment(<<- operator)
+  ## This function is used to inverse a new matrix without running again
+  ## the makeCacheMatrix
+  set <- function(y) {
+    x <<- y
+    invx <<- NULL
+  }
+  
+  ## Function get retrieves matrix x
+  get <- function() x
+  
+  ## Function setinvx(inverse) caches matrix inverse as invx
+  ## in the parent environment
+  setinvx <- function(inverse) invx <<- inverse
+  
+  ##Function getinvx() retrieves inverse matrix invx
+  getinvx <- function() invx
+  
+  ## Generate the list of functions
+  list(set = set, get = get,
+       setinvx = setinvx,
+       getinvx = getinvx)
 }
 
 
-## Write a short comment describing this function
+## Function cacheSolve returns a matrix that is the inverse of x.
+## If the inverse of x has already been computed the function retrieves
+## the inverse from the cache.
+## If the inverse of x is not in the cache, the inverse is computed
+## and stored in the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Test if the inverse of x is cached
+  invx <- x$getinvx()
+  if(!is.null(invx)) {
+    message("getting cached data") 
+  } else {
+    message("computing and caching the inverse matrix")
+    ## Retrieve x into data
+    data <- x$get()
+    ## Compute the inverse matrix into invx
+    invx <- solve(data, ...)
+    ## Put invx in the caches
+    x$setinvx(invx)
+  }
+  ## Return the cached or computed inverse of x
+  return(invx)
 }
